0990812038bea4e90ae454d1fbc5f281f1a390d7
Kim-sangjae <kimsangjae94114@gmail.com>
2025-09-05 16:25:32 +0900

    예제 커밋

--- examples/src/main/java/com/greedy/level01/basic/Application.java
+++ examples/src/main/java/com/greedy/level01/basic/Application.java
@@ -1,6 +1,6 @@
 package com.greedy.level01.basic;
 
-public class CalaulatorMain {
+public class Application {
     public static void main(String[] args) {
         Calculator c = new Calculator();
 
